038dd8651d6932f6fcb9f12b3ed93d280d001719
‘slfeng’ <150@163.com>
2025-06-30 09:10:04 +0800

    feat: 地图相关配置修改

--- src/config/index.js
+++ src/config/index.js
@@ -1,38 +1,3 @@
-export const DEFAULT_CONFIG_bak = {
-  // 百度地图 API Key
-  BAIDU_MAP_KEY: 'Q28k35y40XGOdg9eTLqtWepkqAEMScsv',
-
-  // 地图主题样式 ID
-  MAP_STYLE_ID: '85cd4e58de618944a7ab47f792cb9984',
-
-  // 经度 Longitude
-  LONGITUDE: 122.025963,
-
-  // 纬度 Latitude
-  LATITUDE: 40.724067,
-
-  // 地图缩放级别 Zoom
-  ZOOM: 13,
-
-  // 最小缩放级别 Min Zoom
-  MIN_ZOOM: 10,
-
-  // 最大缩放级别 Max Zoom
-  MAX_ZOOM: 18,
-
-  // 地图倾斜角度 Tilt
-  TILT_ANGLE: 60,
-
-  // 天气 API Key
-  WEATHER_API_KEY: '2xV2vSUweZTZc0rJmuSxl6bniY5qsNco',
-
-  // 天气 API Base URL： https://api.map.baidu.com/weather/v1/
-  WEATHER_API_BASE_URL: 'baiduApi/weather/v1/',
-
-  // 天气 API 请求参数
-  WEATHER_DISTRICT_ID: '211100',
-}
-
 export const DEFAULT_CONFIG = {
   // 百度地图 API Key
   BAIDU_MAP_KEY: 'QSbhc3icqAluRJZHPi00zJGgtKHKzdy6',
